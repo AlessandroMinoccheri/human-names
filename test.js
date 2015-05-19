9d2ea5b33922a41a02f388c7702d7edfceff4738
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-05-19 22:29:57 +0200

    fix test

--- test.js
+++ test.js
@@ -6,10 +6,10 @@ test(function (t) {
     t.assert(humanNames.female.length > 0);
     t.assert(humanNames.male.length > 0);
     t.assert(humanNames.all.length > 0);
-    t.assert(humanNames.femaleRandom());
-    t.assert(humanNames.maleRandom());
-    t.assert(humanNames.allRandom());
-    t.assert(humanNames.allRandom() !== humanNames.allRandom());
+    t.assert(humanNames.femaleRandomEn());
+    t.assert(humanNames.maleRandomEn());
+    t.assert(humanNames.allRandomEn());
+    t.assert(humanNames.allRandomEn() !== humanNames.allRandomEn());
     t.assert(humanNames.all[0] === 'Aaliyah');
     t.assert(humanNames.all[1] === 'Aaron');
     t.end();
