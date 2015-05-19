66264308d7aa4a03729911ed5c72dc177e1a59fa
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-05-19 22:31:56 +0200

    fix test

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ var test = require('ava');
 var humanNames = require('./');
 
 test(function (t) {
-    t.assert(humanNames.female.length > 0);
-    t.assert(humanNames.male.length > 0);
-    t.assert(humanNames.all.length > 0);
+    t.assert(humanNames.femaleEn.length > 0);
+    t.assert(humanNames.maleEn.length > 0);
+    t.assert(humanNames.allEn.length > 0);
     t.assert(humanNames.femaleRandomEn());
     t.assert(humanNames.maleRandomEn());
     t.assert(humanNames.allRandomEn());
     t.assert(humanNames.allRandomEn() !== humanNames.allRandomEn());
-    t.assert(humanNames.all[0] === 'Aaliyah');
-    t.assert(humanNames.all[1] === 'Aaron');
+    t.assert(humanNames.allEn[0] === 'Aaliyah');
+    t.assert(humanNames.allEn[1] === 'Aaron');
     t.end();
 });
