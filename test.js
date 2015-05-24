de19085617b759efc68dedf9e7f92bd8e0b9119b
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-05-24 09:59:29 +0200

    fixed test

--- test.js
+++ test.js
@@ -12,5 +12,25 @@ test(function (t) {
     t.assert(humanNames.allRandomEn() !== humanNames.allRandomEn());
     t.assert(humanNames.allEn[0] === 'Aaliyah');
     t.assert(humanNames.allEn[1] === 'Aaron');
+
+    t.assert(humanNames.femaleIt.length > 0);
+    t.assert(humanNames.maleIt.length > 0);
+    t.assert(humanNames.allIt.length > 0);
+    t.assert(humanNames.femaleRandomIt());
+    t.assert(humanNames.maleRandomIt());
+    t.assert(humanNames.allRandomIt());
+    t.assert(humanNames.allRandomIt() !== humanNames.allRandomIt());
+    t.assert(humanNames.allIt[0] === 'Alessandra');
+    t.assert(humanNames.allIt[1] === 'Alberto');
+
+    t.assert(humanNames.femaleFr.length > 0);
+    t.assert(humanNames.maleFr.length > 0);
+    t.assert(humanNames.allFr.length > 0);
+    t.assert(humanNames.femaleRandomFr());
+    t.assert(humanNames.maleRandomFr());
+    t.assert(humanNames.allRandomFr());
+    t.assert(humanNames.allRandomFr() !== humanNames.allRandomFr());
+    t.assert(humanNames.allFr[0] === 'Aaliyah');
+    t.assert(humanNames.allFr[1] === 'Aaron');
     t.end();
 });
