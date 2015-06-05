1edad05d4a517a2a84d90c8c30b202b256169a56
Alessandro Minoccheri <alessandro@webanddesign.it>
2015-06-05 18:28:27 +0200

    manage spanish name comment

--- cli.js
+++ cli.js
@@ -24,7 +24,7 @@ var cli = meow({
         'Options',
         '  --all   Get all names instead of a random name',
         '  --type  Type of name: female|male|all  Default: all',
-        '  --lang  lang of name: en|it|fr|de  Default: en'
+        '  --lang  lang of name: en|it|fr|de|es  Default: en'
     ].join('\n')
 });
 
