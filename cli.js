ba16ff76e2b4c594df8163420b7be5d437c3b769
Alessandro Minoccheri <alessandro@studiomado.it>
2017-08-20 11:12:42 +0200

    update version to 1.0.9

--- cli.js
+++ cli.js
@@ -24,7 +24,7 @@ var cli = meow({
         'Options',
         '  --all   Get all names instead of a random name',
         '  --type  Type of name: female|male|all  Default: all',
-        '  --lang  lang of name: en|it|fr|de|es  Default: en'
+        '  --lang  lang of name: en|it|fr|de|es|nl  Default: en'
     ].join('\n')
 });
 
